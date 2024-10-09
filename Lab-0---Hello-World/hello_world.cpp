d9979f32a6d3fcf224ebe0029f7478672a27770d
Rick E Morales <97321954+anonymousegoose@users.noreply.github.com>
2024-10-09 16:42:20 -0700

    Hello World

---
+++ Lab-0---Hello-World/hello_world.cpp
@@ -0,0 +1,12 @@
+// hello_world.cpp
+// Rick Morales 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // TODO: Replace "Hello, World!" by "Hello, {your first name}!"
+  cout << "Hello, Ricky" << endl;
+
+  return 0;
+}
